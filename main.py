bd4f21fcc0405ab84c3ad9cbb5f42db1ca10eba5
Nayef <nayefhaidar050@gmail.com>
2025-06-09 19:00:58 +0300

    adding highest grade function

--- main.py
+++ main.py
@@ -5,3 +5,9 @@ def display_student_summary(names, grades):
 
 def get_avg_grade(grades):
     return sum(grades) / len(grades)
+
+
+def get_heighest_grade(names, grades):
+    max_grade = max(grades)
+    index = grades.index(max_grade)
+    return names[index], max_grade
